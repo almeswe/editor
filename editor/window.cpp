c28470c303ecd79e1337cf2d8c33d88d3fa54cbc
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-03-06 01:45:27 +0300

    Update window.cpp

--- editor/window.cpp
+++ editor/window.cpp
@@ -1,30 +1,68 @@
 ﻿#include <windows.h>
 
-#include "DWTextWriter.cpp"
+#include "drawer.h"
+#include "gap.h";
+#include "wchmethods.h"
 
+PAINTSTRUCT ps;
+
+Gap* gap;
 DWTextDrawer* drawer;
-wchar_t* text = const_cast<wchar_t*>(L"privet mir");
+wchar_t* text = const_cast<wchar_t*>(L"");
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (msg)
+    switch (message)
     {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
+        case WM_CREATE:
+            drawer = new DWTextDrawer(window);
+            gap = new Gap();
+            //gap->Insert(9,L"wut??");
+            //gap->Insert(0,L"first");
+            //text = gap->Text;
             break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
+
         case WM_KEYDOWN:
-            //text = (char*)wParam;
+            //rewrite
+            if (wParam == VK_BACK)
+            {
+                text = remlst(text);
+                drawer->DWDrawText(text);
+            }
+            if (GetKeyState(VK_CONTROL) < 0 && GetKeyState(VK_ADD) < 0)
+            {
+                drawer->OnScroll(5.0f);
+                drawer->DWDrawText(text);
+            }
+            if (GetKeyState(VK_CONTROL) < 0 && GetKeyState(VK_SUBTRACT) < 0)
+            {
+                drawer->OnScroll(-5.0f);
+                drawer->DWDrawText(text);
+            }
+            break;
+
+        case WM_CHAR:
+            text = append(text, wParam);
+            drawer->DWDrawText(text);
+            break;
+
+        case WM_SIZE:
+            drawer->OnResize(LOWORD(lParam),HIWORD(lParam));
+            drawer->DWDrawText(text);
             break;
 
         case WM_PAINT:
-        case WM_DISPLAYCHANGE:
+            BeginPaint(window,&ps);
             drawer->DWDrawText(text);
+            EndPaint(window,&ps);
             break;
+
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            break;
+
         default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
+            return DefWindowProc(window, message, wParam, lParam);
     }
     return 0;
 }
@@ -40,12 +78,21 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
     wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 
     RegisterClassEx(&wc);
-    HWND window = CreateWindowEx(WS_EX_CLIENTEDGE, L"ed", L"editor", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, instance, NULL);
-    drawer = new DWTextDrawer(window);
-
+    HWND window = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        L"ed",
+        L"editor",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        instance,
+        NULL
+    );
     ShowWindow(window, SW_SHOW);
-    UpdateWindow(window);
-
     while (GetMessage(&message, NULL, 0, 0) > 0)
     {
         TranslateMessage(&message);
